43ac376234f685314f8497b7782d56018b97b48d
0x-voidptr <0x.voidweaver@gmail.com>
2022-09-05 15:12:29 +0800

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using static FollowerTrait.TraitType;
+
+namespace NothingNegative
+{
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        } 
+    }
+
+    [HarmonyPatch(typeof(FollowerBrain))]
+    [HarmonyPatch(MethodType.Constructor)]
+    [HarmonyPatch(new Type[] { typeof(FollowerInfo) })]
+    public class Patch_FollowerBrain
+    {
+        static void Postfix(FollowerBrain __instance)
+        {
+            Console.WriteLine($"Updating follower with {__instance._directInfoAccess.Traits.Count} traits");
+            for (int i = 0; i < __instance._directInfoAccess.Traits.Count; i++)
+            {
+                Console.WriteLine($"Updating trait {__instance._directInfoAccess.Traits[i]}");
+                __instance._directInfoAccess.Traits[i] = __instance._directInfoAccess.Traits[i] switch
+                {
+                    Germophobe => Coprophiliac,
+                    FearOfSickPeople => LoveOfSickPeople,
+                    Cynical => Gullible,
+                    Libertarian => Disciplinarian,
+                    Sickly => IronStomach,
+                    NaturallySkeptical => NaturallyObedient,
+                    AgainstSacrifice => SacrificeEnthusiast,
+                    Faithless => Faithful,
+                    Lazy => Industrious,
+                    FearOfDeath => DesensitisedToDeath,
+                    _ => __instance._directInfoAccess.Traits[i]
+                };
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# NothingNegative: A COTL Mod
+
+Replaces negative traits with positive equivalents
+
+---
+
+When indoctrinating a new follower, the negative traits that they have will be replaced with positive equivalents.
+
+This effect will be permanent for any followers indoctrinated with the mod installed!
